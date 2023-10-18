ac01ba01912d93da7e9cfff36eb0a80529aa352c
GerarC <gcastillo.m14@gmail.com>
2023-10-18 14:52:28 -0500

    Initial Commit

---
+++ src/main/java/co/edu/udea/SalasInfo/SalasInfoApplication.java
@@ -0,0 +1,13 @@
+package co.edu.udea.SalasInfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalasInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalasInfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/udea/SalasInfo/SalasInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.udea.SalasInfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalasInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
